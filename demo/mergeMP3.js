8ed24142d78f602d52a4fd84721a341baf7f1600
Your Name <27681135@qq.com>
2020-02-15 12:19:29 +0800

    合并，字幕和计时完成

--- demo/mergeMP3.js
+++ demo/mergeMP3.js
@@ -4,13 +4,6 @@ const path = require("path");
 const { readdir, readFile, writeFile, readdirSync, statSync } = require("fs");
 const { getStringList } = require("./readText");
 
-// var firstFile = "../mp3/1.mp3";
-// var secondFile = "../mp3/2.mp3";
-
-var outPath = path.join(__dirname, "../merged/result.mp3");
-let mp3path = path.join(__dirname, "../result");
-let srtOutputPath = path.join(__dirname, "../merged/result.srt");
-
 function pReadFile(filepath) {
   return new Promise((resolve, reject) => {
     readdir(filepath, (err, files) => {
@@ -34,15 +27,16 @@ function pReadFile(filepath) {
 }
 
 function mergeAllMp3(srtOffset = 0) {
+  let tempVoiceFolder = global.config.tempVoiceFolder;
+  let pathMergedVoices = global.config.pathMergedVoices;
   return new Promise(async (resolve, reject) => {
-    let filenames = await pReadFile(mp3path);
+    let filenames = await pReadFile(tempVoiceFolder);
     let proc = ffmpeg();
     let lists = [];
     for (let index = 0; index < filenames.length; index++) {
-      let file = path.join(__dirname, "../result", filenames[index]);
+      let file = path.join(tempVoiceFolder, filenames[index]);
       await proc.input(file).ffprobe(function(err, data) {
-        console.log("file metadata:");
-        console.log(data.format.duration);
+        // console.log("file metadata:", data.format.duration);
         let obj = {
           duration: data.format.duration,
           filename: filenames[index].replace(".mp3", "")
@@ -53,14 +47,14 @@ function mergeAllMp3(srtOffset = 0) {
     // console.log(lists)
     proc
       .on("end", function() {
-        console.log("files have been merged succesfully");
         let total = formatLists(lists, srtOffset);
+        console.log("files have been merged succesfully. ");
         resolve(total);
       })
       .on("error", function(err) {
         console.log("an error happened: " + err.message);
       })
-      .mergeToFile(outPath);
+      .mergeToFile(pathMergedVoices);
   });
 }
 
@@ -81,14 +75,17 @@ ${row.content}
 
 `;
   }
-  console.log(lists);
+  // console.log(lists);
   await writeSrt(strResult);
+  total = Math.round(total * 1000) / 1000;
+  console.log("总时长秒数：" + total);
   return total;
 }
 
 function writeSrt(str) {
+  let pathSrt = global.config.pathSrt;
   return new Promise(async (resolve, reject) => {
-    writeFile(srtOutputPath, str, function(err) {
+    writeFile(pathSrt, str, function(err) {
       if (err) {
         console.error(err);
         reject(err);
