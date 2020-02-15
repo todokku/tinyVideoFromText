16df19f97ce9e435f67aaf51e8bf60c09f74bf77
Your Name <27681135@qq.com>
2020-02-15 09:26:38 +0800

    测试配置全局路径，开新分支

--- demo/init.js
+++ demo/init.js
@@ -1,4 +1,5 @@
 const utils = require("./utils");
+const path = require("path");
 
 function clearPreviousFiles() {
   utils.rmAllFiles("../result");
@@ -6,4 +7,32 @@ function clearPreviousFiles() {
   console.log("清除临时文件成功");
 }
 
+function selectPath(dir = path.join(__dirname, "..")) {
+  let mainPath = path.resolve(dir);
+  let bgmInputFolder = path.join(mainPath, "bgm");
+  let bgmInput = path.join(bgmInputFolder, "bgm.mp3");
+  let picInputFolder = path.join(mainPath, "pic");
+  let tempVoiceFolder = path.join(mainPath, "result");
+  let tempResultFolder = path.join(mainPath, "merged");
+  let pathSrt = path.join(tempResultFolder, "result.srt");
+  let pathMergedVoices = path.join(tempResultFolder, "mergedVoice.mp3");
+  let pathVoiceAndPic = path.join(tempResultFolder, "voiceAndPic.mp4");
+  let pathFinal = path.join(tempResultFolder, "final.mp4");
+
+  const config = {
+    bgmInputFolder,
+    bgmInput,
+    picInputFolder,
+    tempVoiceFolder,
+    tempResultFolder,
+    pathSrt,
+    pathMergedVoices,
+    pathVoiceAndPic,
+    pathFinal
+  };
+  console.log(config);
+  return config;
+}
+
+exports.selectPath = selectPath;
 exports.clearPreviousFiles = clearPreviousFiles;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { bgRename } = require("./demo/utils");
 
-const { clearPreviousFiles } = require("./demo/init");
+const { selectPath, clearPreviousFiles } = require("./demo/init");
 const { readAll } = require("./demo/readText");
 const { merge } = require("./demo/mergeMP3");
 const { addBgm, generateBGM } = require("./demo/addBgm");
@@ -8,6 +8,7 @@ const { addJpg } = require("./demo/addJpg");
 const { addText } = require("./demo/addText");
 
 async function main() {
+  let t = selectPath("./test");
   // bgRename("./test");
   // let time = 180;
   // clearPreviousFiles();
